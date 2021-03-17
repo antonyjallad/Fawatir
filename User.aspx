46b81036be9ad7cf478886912e7c16a7b6a0d64d
AnthonyJallad <abj00001@students.aust.edu.lb>
2021-03-18 00:56:21 +0200

    User

---
+++ User.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="User.aspx.cs" Inherits="Fawatir.User" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 560px;
+            height: 132px;
+        }
+        .auto-style2 {
+            width: 675px;
+            height: 343px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+               <div class="auto-style1">
+            &nbsp;Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox2" runat="server" TextMode="Email"></asp:TextBox>
+                    <br />
+             Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox1" runat="server" TextMode="Password"></asp:TextBox>
+                   <br />
+                   <br />
+                   <asp:Button ID="Button1" runat="server" Text="Login" OnClick="Button1_Click1" />
+                   <br />
+                   </div>
+                <div>
+            <div class="auto-style2">
+                First Name<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                <br />
+                Middle Name<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                <br />
+                Last Name<asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                <br />
+                Phone Number<asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+                <br />
+                Email<asp:TextBox ID="TextBox7" runat="server" TextMode="Email"></asp:TextBox>
+                <br />
+                Password<asp:TextBox ID="TextBox8" runat="server" TextMode="Password"></asp:TextBox>
+                <br />
+                Confirm Password<asp:TextBox ID="TextBox9" runat="server" TextMode="Password"></asp:TextBox>
+                   <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToCompare="TextBox8" ControlToValidate="TextBox9" ErrorMessage="Confirm Password does not match" ForeColor="Red"></asp:CompareValidator>
+                <br />
+                   <asp:Button ID="Button2" runat="server" Text="Create Account" OnClick="Button2_Click1" />
+                    </div>  
+                        </form>
+
+                </body>
+</html>
---
+++ User.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Fawatir
+{
+    public partial class User : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button2_Click1(object sender, EventArgs e)
+        {
+            if (TextBox3.Text == string.Empty || TextBox4.Text == string.Empty || TextBox5.Text == string.Empty || TextBox6.Text == string.Empty || TextBox7.Text == string.Empty || TextBox8.Text == string.Empty || TextBox9.Text == string.Empty)
+            {
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alertMessage", "alert('All Fields Should be Filled!')", true);
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString());
+                    conn.Open();
+                    string sql2 = "Select count(1) from Users where email=@email";
+                    SqlCommand com = new SqlCommand(sql2, conn);
+                    com.Parameters.AddWithValue("@email", TextBox7.Text);
+                    com.ExecuteNonQuery();
+                    SqlDataReader sdr = com.ExecuteReader();
+                    int count = 0;
+                    while (sdr.Read())
+                    {
+                        count = sdr.GetInt32(0);
+                    }
+                    if (count == 0)
+                    {
+                        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString());
+                        con.Open();
+                        string sql = "INSERT INTO Users (fn, mn, ln, pn, email, password) VALUES (@fn, @mn, @ln, @pn, @email, @pass)";
+
+                        SqlCommand comm = new SqlCommand(sql, con);
+                        comm.Parameters.AddWithValue("@fn", TextBox3.Text);
+                        comm.Parameters.AddWithValue("@mn", TextBox4.Text);
+                        comm.Parameters.AddWithValue("@ln", TextBox5.Text);
+                        comm.Parameters.AddWithValue("@pn", TextBox6.Text);
+                        comm.Parameters.AddWithValue("@email", TextBox7.Text);
+                        comm.Parameters.AddWithValue("@pass", TextBox8.Text);
+                        comm.ExecuteNonQuery();
+                        con.Close();
+                        Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alertMessage", "alert('Account Created, Now Please Login')", true);
+                    }
+                    else
+                    {
+                        Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alertMessage", "alert('Email already used')", true);
+                    }
+                    conn.Close();
+                }
+                catch (SqlException ex)
+                {
+                    Response.Write("Error occured: " + ex.Message);
+                }
+            }
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == string.Empty || TextBox2.Text == string.Empty)
+            {
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alertMessage", "alert('All Fields Should be Filled!')", true);
+            }
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString()))
+                {
+
+                    conn.Open();
+
+                    string sql1 = "SELECT * FROM Users WHERE Email = @email AND Password = @pass";
+
+                    using (SqlCommand comm = new SqlCommand(sql1, conn))
+                    {
+                        comm.Parameters.AddWithValue("@email", TextBox2.Text);
+                        comm.Parameters.AddWithValue("@pass", TextBox1.Text);
+
+                        using (SqlDataReader reader = comm.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Session["UserEmail"] = TextBox2.Text;
+                                Session["UserEmailID"] = TextBox2.Text;
+
+                                /* Session["UserID"] = reader["RegisterID"];
+                                 Session["UserEmailID"] = txtEmail.Text;
+                                 Session["Email"] = txtEmail.Text;
+                                 Session["name"] = reader["Name"];
+                                 Session["pp"] = reader["ProfilePic"];
+                                 string MyID = Session["UserID"].ToString();
+                                 Session["CurrentProfileId"] = Session["UserID"];*/
+                                Response.Redirect("UserHomePage.aspx");
+                            }
+                            else
+                            {
+                                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alertMessage", "alert('Incorrect Email or Password!.')", true);
+                            }
+                        }
+                    }
+
+                    conn.Close();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("Error occured: " + ex.Message);
+            }
+        }
+    }
+}
