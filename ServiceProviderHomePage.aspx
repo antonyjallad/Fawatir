00f90b0ce645d223527be18800ab7f51c3d78e3a
AnthonyJallad <abj00001@students.aust.edu.lb>
2021-03-18 00:55:38 +0200

    Provider Home Page

---
+++ ServiceProviderHomePage.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ServiceProviderHomePage.aspx.cs" Inherits="Fawatir.ServiceProviderHomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Email
+            <asp:TextBox ID="TextBox1" runat="server" TextMode="Email"></asp:TextBox>
+            <br />
+            Password<asp:TextBox ID="TextBox2" runat="server" TextMode="Password"></asp:TextBox>
+        </div>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Login" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ ServiceProviderHomePage.aspx.cs
@@ -0,0 +1,64 @@
+﻿                using System;
+                using System.Collections.Generic;
+                using System.Configuration;
+                using System.Data.SqlClient;
+                using System.Linq;
+                using System.Web;
+                using System.Web.UI;
+                using System.Web.UI.WebControls;
+
+namespace Fawatir
+{
+    public partial class ServiceProviderHomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCnx = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString()))
+                {
+                    sqlCnx.Open();
+
+                    string sql1 = "SELECT * FROM ServicesProvider WHERE Email = @email AND Password = @password";
+
+                    using (SqlCommand comm = new SqlCommand(sql1, sqlCnx))
+                    {
+                        comm.Parameters.AddWithValue("@email", TextBox1.Text);
+                        comm.Parameters.AddWithValue("@password", TextBox2.Text);
+
+                        using (SqlDataReader reader = comm.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Session["Email"] = TextBox1.Text;
+                                Session["EmailID"] = TextBox1.Text;
+                                Session["Type"] = reader.GetString(4);
+                                /*Session["UserID"] = reader["RegisterID"];
+                                Session["name"] = reader["Name"];
+                                string MyID = Session["UserID"].ToString();
+                                Session["CurrentProfileId"] = Session["UserID"];*/
+                                Response.Redirect("ProfilePage.aspx");
+                            }
+                            else
+                            {
+                                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alertMessage", "alert('Incorrect Email or Password!.')", true);
+                            }
+                        }
+                    }
+
+                    sqlCnx.Close();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("Error occured: " + ex.Message);
+            }
+        }
+    }
+}
