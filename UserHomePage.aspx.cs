05c663d962b2de841e4d38c9f2a108ac21d8c30a
AnthonyJallad <abj00001@students.aust.edu.lb>
2021-03-18 00:56:53 +0200

    User Home Page

---
+++ UserHomePage.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Fawatir
+{
+    public partial class UserHomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserEmailID"] == null)
+            {
+                Response.Redirect("User.aspx");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Session["UserEmailID"] != null)
+            {
+                Session["UserEmailID"] = null;
+                Response.Redirect("User.aspx");
+            }
+        }
+    }
+}
