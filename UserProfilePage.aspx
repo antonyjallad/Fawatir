58298a217c4f4491ca26c654e402ce0ff5adfc5c
AnthonyJallad <abj00001@students.aust.edu.lb>
2021-03-18 00:57:41 +0200

    User Profile Page

---
+++ UserProfilePage.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserProfilePage.aspx.cs" Inherits="Fawatir.UserProfilePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <table class="UserInfo">
+            <tr>
+                <td><asp:Label ID="Label1" runat="server" Text="First Name" Font-Bold="True"></asp:Label></td>
+                <td><asp:Label ID="lblFn" runat="server" Text=""></asp:Label></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="Label2" runat="server" Text="Last Name" Font-Bold="True"></asp:Label></td>
+                <td><asp:Label ID="lblLn" runat="server" Text=""></asp:Label></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="Label3" runat="server" Text="Middle Name" Font-Bold="True"></asp:Label></td>
+                <td><asp:Label ID="lblMn" runat="server" Text=""></asp:Label></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="Label4" runat="server" Text="Email" Font-Bold="True"></asp:Label></td>
+                <td><asp:Label ID="lblEmail" runat="server" Text=""></asp:Label></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="Label6" runat="server" Text="Phone Number" Font-Bold="True"></asp:Label></td>
+                <td><asp:Label ID="lblPn" runat="server" Text=""></asp:Label></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ UserProfilePage.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Fawatir
+{
+    public partial class UserProfilePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserEmailID"] == null)
+            {
+                Response.Redirect("User.aspx");
+            }
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString()))
+            {
+                conn.Open();
+                string sql1 = "SELECT * FROM Users WHERE email = @email";
+
+                using (SqlCommand comm = new SqlCommand(sql1, conn))
+                {
+                    comm.Parameters.AddWithValue("@email", Session["UserEmail"].ToString());
+                    using (SqlDataReader reader = comm.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            lblFn.Text = reader.GetString(1);
+                            lblLn.Text = reader.GetString(3);
+                            lblMn.Text = reader.GetString(2);
+                            lblEmail.Text = reader.GetString(5);
+                            lblPn.Text = reader.GetString(4);
+                        }
+                    }
+                }
+                conn.Close();
+            }
+        }
+    }
+}
