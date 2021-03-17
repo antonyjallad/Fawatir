94b509b94f737853e875c22d8fd83e83396daefb
AnthonyJallad <abj00001@students.aust.edu.lb>
2021-03-18 00:54:12 +0200

    New Service

---
+++ NewService.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NewService.aspx.cs" Inherits="Fawatir.NewService" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label9" runat="server" Text="Label"></asp:Label>
+            <br />
+            Provider:
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+&nbsp;<asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+&nbsp;<asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+&nbsp;<br />
+            Email:
+            <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+            <br />
+            Phone Number:
+            <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+            <br />
+        </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label7" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label8" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+            <asp:Label ID="Label10" runat="server"></asp:Label>
+            &nbsp;<asp:Label ID="Label11" runat="server"></asp:Label>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Log Out" />
+    </form>
+</body>
+</html>
---
+++ NewService.aspx.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Fawatir
+{
+    public partial class NewService : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["EmailID"] == null)
+            {
+                Response.Redirect("HomePage.aspx");
+            }
+            if (Session["Type"].ToString()=="Internet")
+            {
+                
+                string sql = "SELECT * FROM ServicesProvider WHERE email = @email";
+                String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection sqlCnx = new SqlConnection(connectionString);
+                sqlCnx.Open();
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlCnx);
+                sqlCommand.Parameters.AddWithValue("@email", Session["email"].ToString());
+                SqlDataReader sdr = sqlCommand.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Label2.Text = sdr.GetString(1);
+                    Label3.Text = sdr.GetString(3);
+                    Label4.Text=sdr.GetString(2);
+                    Label5.Text = sdr.GetString(7);
+                    Label6.Text = sdr.GetString(5);
+                }
+             
+                Label7.Text = "Quota Limit";
+                Label8.Text = "Price";
+            }
+            else if (Session["Type"].ToString() == "Electricity")
+            {
+
+                string sql = "SELECT * FROM ServicesProvider WHERE email = @email";
+                String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection sqlCnx = new SqlConnection(connectionString);
+                sqlCnx.Open();
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlCnx);
+                sqlCommand.Parameters.AddWithValue("@email", Session["email"].ToString());
+                SqlDataReader sdr = sqlCommand.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Label2.Text = sdr.GetString(1);
+                    Label3.Text = sdr.GetString(3);
+                    Label4.Text = sdr.GetString(2);
+                    Label5.Text = sdr.GetString(7);
+                    Label6.Text = sdr.GetString(5);
+                }
+
+                Label7.Text = "AMP";
+                Label8.Text = "Maintenance";
+                Label9.Text = "Electricity Service";
+                Label10.Text = "Unit Price";
+                Label11.Text = "730";
+            }
+            if (Session["Type"].ToString() == "Water")
+            {
+
+                string sql = "SELECT * FROM ServicesProvider WHERE email = @email";
+                String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection sqlCnx = new SqlConnection(connectionString);
+                sqlCnx.Open();
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlCnx);
+                sqlCommand.Parameters.AddWithValue("@email", Session["email"].ToString());
+                SqlDataReader sdr = sqlCommand.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Label2.Text = sdr.GetString(1);
+                    Label3.Text = sdr.GetString(3);
+                    Label4.Text = sdr.GetString(2);
+                    Label5.Text = sdr.GetString(7);
+                    Label6.Text = sdr.GetString(5);
+                }
+
+                Label7.Text = "Litre";
+                Label8.Text = "Price";
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Session["Type"].ToString() == "Internet")
+            {
+                int price = Convert.ToInt32(TextBox2.Text);
+                int limit = Convert.ToInt32(TextBox1.Text);
+                string sql = "insert into InternetServices(price,limit,pfn,pln,pmn,ppn,pe)values(@p,@l,@pfn,@pln,@pmn,@ppn,@pe)";
+                String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection sqlCnx = new SqlConnection(connectionString);
+                sqlCnx.Open();
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlCnx);
+                sqlCommand.Parameters.AddWithValue("@p", price);
+                sqlCommand.Parameters.AddWithValue("@l", limit);
+                sqlCommand.Parameters.AddWithValue("@pfn", Label2.Text);
+                sqlCommand.Parameters.AddWithValue("@pln", Label4.Text);
+                sqlCommand.Parameters.AddWithValue("@pmn", Label3.Text);
+                sqlCommand.Parameters.AddWithValue("@ppn", Label5.Text);
+                sqlCommand.Parameters.AddWithValue("@pe", Label6.Text);
+                sqlCommand.ExecuteNonQuery();
+
+                Response.Redirect("ProfilePage.aspx");
+
+
+            }
+            else if (Session["Type"].ToString() == "Electricity")
+            {
+                int  price= Convert.ToInt32(Label11.Text);
+
+                int main = Convert.ToInt32(TextBox2.Text);
+                int amp = Convert.ToInt32(TextBox1.Text);
+                string sql = "insert into electricityservices(amp,main,price,pfn,pln,pmn,ppn,pe)values(@a,@m,@p,@pfn,@pln,@pmn,@ppn,@pe)";
+                String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection sqlCnx = new SqlConnection(connectionString);
+                sqlCnx.Open();
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlCnx);
+                sqlCommand.Parameters.AddWithValue("@a", amp);
+                sqlCommand.Parameters.AddWithValue("@m", main);
+                sqlCommand.Parameters.AddWithValue("@p", price);
+                sqlCommand.Parameters.AddWithValue("@pfn", Label2.Text);
+                sqlCommand.Parameters.AddWithValue("@pln", Label4.Text);
+                sqlCommand.Parameters.AddWithValue("@pmn", Label3.Text);
+                sqlCommand.Parameters.AddWithValue("@ppn", Label5.Text);
+                sqlCommand.Parameters.AddWithValue("@pe", Label6.Text);
+                sqlCommand.ExecuteNonQuery();
+
+                Response.Redirect("ProfilePage.aspx");
+
+            }
+            else if (Session["Type"].ToString() == "Water")
+            {
+                int price = Convert.ToInt32(TextBox2.Text);
+                int litre = Convert.ToInt32(TextBox1.Text);
+                string sql = "insert into waterservices(litre,price,pfn,pln,pmn,ppn,pe)values(@l,@p,@pfn,@pln,@pmn,@ppn,@pe)";
+                String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                SqlConnection sqlCnx = new SqlConnection(connectionString);
+                sqlCnx.Open();
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlCnx);
+                sqlCommand.Parameters.AddWithValue("@l", litre);
+                sqlCommand.Parameters.AddWithValue("@p", price);
+                sqlCommand.Parameters.AddWithValue("@pfn", Label2.Text);
+                sqlCommand.Parameters.AddWithValue("@pln", Label4.Text);
+                sqlCommand.Parameters.AddWithValue("@pmn", Label3.Text);
+                sqlCommand.Parameters.AddWithValue("@ppn", Label5.Text);
+                sqlCommand.Parameters.AddWithValue("@pe", Label6.Text);
+                sqlCommand.ExecuteNonQuery();
+
+                Response.Redirect("ProfilePage.aspx");
+
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (Session["EmailID"] != null)
+            {
+                Session["EmailID"] = null;
+                Response.Redirect("HomePage.aspx");
+            }
+        }
+    }
+}
